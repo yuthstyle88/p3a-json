dac87dc7645f2a5bde4c16c2dc55a3de9a5946d6
yongyutjantaboot <yuth_style88@hotmail.com>
2025-06-18 14:14:25 +0700

    add mockup

--- src/constellation/routes/instances.rs
+++ src/constellation/routes/instances.rs
@@ -3,6 +3,6 @@ use crate::constellation::handlers::instances::{process_instances_info, process_
 
 pub fn instances_scope() -> Scope {
     web::scope("/instances")
-        .route("/{speed}/info", web::post().to(process_instances_info))
+        .route("/{speed}/info", web::get().to(process_instances_info))
         .route("/{speed}/randomness", web::post().to(process_instances_randomness))
 }
